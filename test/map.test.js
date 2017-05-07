842bd66daf8c60a8d985a82bce97fcb999207062
Kirk Rohani <kirk.rohani@gmail.com>
2017-05-07 15:54:01 -0700

    completed map and pluck tests

--- test/map.test.js
+++ test/map.test.js
@@ -6,4 +6,49 @@ describe('map()', () => {
     const mappedArr = _.map(arr, (el) => el * el);
     expect(mappedArr).toEqual([1, 4, 9, 16, 25]);
   });
+
+  it('maps every numbers in an object of number values to their half', () => {
+    const obj = {
+      "number1":  1,
+      "numebr2": 2,
+      "number3": 3,
+      "number4": 4
+    };
+
+    const mappedArr = _.map(obj, (el) => el / 2);
+    expect(mappedArr).toEqual([.5,1,1.5,2]);
+  });
+
+  it('reformats objects in an array', () => {
+    const arr = [
+      {"age": 12},
+      {"age": 24},
+      {"age": 36},
+      {"age": 48}
+    ];
+
+    const mappedArr = _.map(arr, (obj) => {
+      if (obj.age < 18) {
+        return {"age": "just a baby"};
+      } else if (obj.age < 25) {
+        return {"age": "teenager"};
+      } else if (obj.age < 40) {
+        return {"age": "mature adult"};
+      } else if (obj.age < 70) {
+        return {"age": "senior citizen"};
+      } else {
+        return {"age": "retirement time"};
+      }
+    });
+
+    const newArr = [
+      {"age": "just a baby"},
+      {"age": "teenager"},
+      {"age": "mature adult"},
+      {"age": "senior citizen"}
+    ];
+
+    expect(mappedArr).toEqual(newArr);
+  });
+
 });
--- test/pluck.test.js
+++ test/pluck.test.js
@@ -1,6 +1,7 @@
 const _ = require('../underbar');
 
 describe('pluck()', () => {
+
   it('returns an array of just ages, given an array of people objects', () => {
     const people = [
       { name: 'Harriet', age: 12},
@@ -9,6 +10,23 @@ describe('pluck()', () => {
     ];
     const result = _.pluck(people, 'age');
     expect(result).toEqual([12, 999, 14]);
+  });
+
+  it('returns back original array if an array of non-objects is passed in', () => {
+    const people = ["a", "b", "c", "d"];
+    const result = _.pluck(people, 0);
+    expect(result).toEqual(people);
+  });
+  
+  
+  it('returns array of undefined values if key does not exist in objects in array', () => {
+    const people = [
+      { name: 'Harriet', age: 12},
+      { name: 'Lazarus', age: 999},
+      { name: 'Bethany', age: 14}
+    ];
+    const result = _.pluck(people, "height");
+    expect(result).toEqual([undefined, undefined, undefined]);
   });
 
 });
